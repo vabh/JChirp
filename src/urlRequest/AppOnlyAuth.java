7ab2a67b12e18b682d2ae7e7d3fd605fd90b2cbb
vabh <anuvabhdutt@gmail.com>
2015-03-20 22:19:05 +0100

    App-Only GET working

--- src/urlRequest/AppOnlyAuth.java
+++ src/urlRequest/AppOnlyAuth.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,7 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -25,17 +27,19 @@ import org.json.JSONObject;
 public class AppOnlyAuth {
 
 	private String consumerKey;
-	private String consumerSecret;
-	private String authURL = "https://api.twitter.com/oauth2/token";
+	private String consumerSecret;	
 	
-	private String accessToken;
-	private boolean authenticated;
+	private String authURL = "https://api.twitter.com/oauth2/token";
+	private String invalidateURL = "https://api.twitter.com/oauth2/invalidate_token";
+
+	private String accessToken = "AAAAAAAAAAAAAAAAAAAAAK99bwAAAAAAmR3yMIxuSvydamKRNehBxjRqPQ4%3DDWtT4gPOkITK6iRDGQnALvRmn0kWqO1QhAoiyL07MPrfz7jgjZ";
+	private boolean authenticated = true ;
 
 	public AppOnlyAuth(String consumerKey, String consumerSecret) {
 		this.consumerKey = consumerKey;
 		this.consumerSecret = consumerSecret;
-		
-		accessToken = "";
+
+		accessToken = null;
 		authenticated = false;
 	}
 
@@ -49,13 +53,13 @@ public class AppOnlyAuth {
 				+ jsonString + "]";
 		return new TwitterJSON(jsonString);
 	}
-	
-	public String getKeyValue(String json, String key) throws org.json.JSONException {
+
+	public String getKeyValue(String json, String key)
+			throws org.json.JSONException {
 		return new JSONObject(json).getString(key);
 	}
 
-	public String authenticate() throws ClientProtocolException,
-			IOException {
+	public String authenticate() throws ClientProtocolException, IOException {
 
 		String consumerKeyURLEncode = URLEncoder.encode(consumerKey, "UTF-8");
 		String consumerSecretURLEncode = URLEncoder.encode(consumerSecret,
@@ -100,45 +104,126 @@ public class AppOnlyAuth {
 		}
 		return responseData;
 	}
-	
-	public void getAccessToken(){
-		
-		if(authenticated)
+
+	public void getAccessToken() {
+
+		if (authenticated)
 			return;
-		try{
+		try {
 			String authResponse = authenticate();
-			try{				 
+			try {
 				accessToken = getKeyValue(authResponse, "access_token");
 				authenticated = true;
-			}
-			catch(org.json.JSONException e){
+			} catch (org.json.JSONException e) {
 				e.printStackTrace();
 				System.out.println("Invalid credentials");
 			}
-		}
-		catch(ClientProtocolException e)
-		{	
-			e.printStackTrace();			
-		}
-		catch(IOException e){
+		} catch (ClientProtocolException e) {
 			e.printStackTrace();
-		}		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String invalidateToken() throws  ClientProtocolException, IOException {
+		
+		String consumerKeyURLEncode = URLEncoder.encode(consumerKey, "UTF-8");
+		String consumerSecretURLEncode = URLEncoder.encode(consumerSecret,
+				"UTF-8");
+
+		String bearerTokenCredentials = consumerKeyURLEncode + ":"
+				+ consumerSecretURLEncode;
+
+		String base64enc = new String(
+				Base64.encodeBase64(bearerTokenCredentials.getBytes()));
+
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpPost httppost = new HttpPost(invalidateURL);
+
+		List<NameValuePair> formparams = new ArrayList<NameValuePair>();		
+		formparams.add(new BasicNameValuePair("access_token", accessToken));
+		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams,
+				Consts.UTF_8);
+
+		httppost.setEntity(entity);
+		httppost.setHeader("Authorization", "Basic " + base64enc);
+		httppost.setHeader("Content-Type",
+				"application/x-www-form-urlencoded;charset=UTF-8");
+
+		CloseableHttpResponse response = httpclient.execute(httppost);
+
+		String responseData = null;
+		try {
+			HttpEntity entityRes = response.getEntity();
+			if (entityRes != null) {
+				InputStream instream = entityRes.getContent();
+				try {
+					responseData = EntityUtils.toString(entityRes);
+				} finally {
+					instream.close();
+					EntityUtils.consume(entity);
+				}
+			}
+		} finally {
+			response.close();
+		}
+		return responseData;
+	}
+
+	public String get(String url) throws ClientProtocolException, IOException {
+
+		if(!authenticated)
+			//throw exception
+			return null;
+		
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+
+		HttpGet httpget = new HttpGet(url);
+		httpget.setHeader("Authorization", "Bearer " + accessToken);
+		httpget.setHeader("Accept-Encoding", "gzip");
+
+		CloseableHttpResponse response = httpclient.execute(httpget);
+		String responseData = null;
+
+		try {
+			HttpEntity entity = response.getEntity();
+			if (entity != null) {
+				InputStream instream = entity.getContent();
+				try {
+					responseData = EntityUtils.toString(entity);
+				} finally {
+					instream.close();
+				}
+			}
+		} finally {
+			response.close();
+		}
+		return responseData;
 	}
 
 	public static void main(String args[]) throws IOException {
 		BufferedReader credentialsFile = new BufferedReader(new FileReader(
 				"credentials.txt"));
 		AppOnlyAuth TwitterQuery;
-		
+
 		try {
 			TwitterQuery = new AppOnlyAuth(credentialsFile.readLine(),
 					credentialsFile.readLine());
 
-			TwitterQuery.getAccessToken();			
-			
+			TwitterQuery.getAccessToken();
+
 			System.out.println(TwitterQuery.accessToken);
 			
-		} finally {
+			String url = "https://api.twitter.com/1.1/statuses/user_timeline.json?count=10&screen_name=twitterapi";
+			
+			System.out.println(TwitterQuery.getJSON(TwitterQuery.get(url)));
+			System.out.println(TwitterQuery.getJSON(TwitterQuery.invalidateToken()));
+
+		} 
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally {
 			credentialsFile.close();
 		}
 	}
