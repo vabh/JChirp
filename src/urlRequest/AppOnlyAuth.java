64d877e4ba9be08ee5e7194a190fd951f8bd53a8
mourjo <sen.mourjo@etu.unice.fr>
2015-03-21 21:23:43 +0100

    unused import removed

--- src/urlRequest/AppOnlyAuth.java
+++ src/urlRequest/AppOnlyAuth.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
